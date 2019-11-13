4adc7d5a0c3a3d88be25f341f029ab4f19c56958
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-13 23:32:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,8 +52,10 @@ class Dog
   end
   
   def self.new_from_db(row)
-    hash = {id: row[0], name: row[1], breed: row[2]}
-    self.new(hash)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(self.id, self.name, self.breed)
   end
   
   def self.find_by_id(id)
